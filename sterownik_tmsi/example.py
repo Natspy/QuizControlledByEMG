b69de36d9d5c36e1ede014bf55c6ebb503ecda57
Piotr Biegański <p.bieganski@student.uw.edu.pl>
2022-03-03 21:13:11 +0100

    wczytywanie plików

--- sterownik_tmsi/example.py
+++ sterownik_tmsi/example.py
@@ -9,9 +9,11 @@ amp.start_sampling()
 gains = np.array(amp.current_description.channel_gains)
 offsets = np.array(amp.current_description.channel_offsets)
 
+
 def samples_to_microvolts(samples):  # z jednostek wzmacniacza do mikrowoltów
     return samples * gains + offsets
-    
+
+
 while True:
     # 16 próbek w pakiecie, nieodebrane próbki się bufurują i można odebrać je później
     packet = amp.get_samples(16)
